68e3a18256ac5748a699abfae1c9cfa074f4d7bc
liming <liming32@baidu.com>
2022-06-27 17:28:16 +0800

    feat: add redirect router

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
 const Koa = require('koa');
+// const Router = require('@koa/router')
 const serve = require('koa-static');
 const app = new Koa();
+// const router = new Router();
 const PORT = process.env.PORT || 3888;
 
-app.use(serve(__dirname + "/public", {extensions: ["html"]}))
-app.listen(PORT, function() {
+// router.redirect("*","/");
+const redirect = async function (ctx, next) {
+    if (ctx.request.url === '/quant/grid') {
+        ctx.redirect('/');
+    }
+    await next();
+}
+app.use(redirect);
+app.use(serve(__dirname + "/public", { extensions: ["html"] }))
+
+
+app.listen(PORT, function () {
     console.log('koa server running at port', PORT);
 })
